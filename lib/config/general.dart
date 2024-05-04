90533ebe0ed0e0c0f161388d93dac6fb8f422138
bmceachnie <60017181+McQuenji@users.noreply.github.com>
2024-05-04 19:49:52 +0200

    chore: setup packages
    Installed flutter_modular and riverpod and set up a basic blank page

---
+++ lib/config/general.dart
@@ -0,0 +1,11 @@
+/// Contains general configurations for the app.
+library confgig.general;
+
+/// The name of the app.
+/// Defaults to "McQuenji's Shopping List".
+///
+/// This value can be overridden by setting the `APP_NAME` environment variable.
+const kAppName = String.fromEnvironment(
+  "APP_NAME",
+  defaultValue: "McQuenji's Shopping List",
+);
