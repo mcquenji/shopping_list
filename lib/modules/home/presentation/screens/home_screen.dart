8b3a12d2bd9fca944b55889be67ec02e402f066d
McQuenji <60017181+mcquenji@users.noreply.github.com>
2024-05-22 03:54:33 +0200

    fix: clean up shopping lists layout

--- lib/modules/home/presentation/screens/home_screen.dart
+++ lib/modules/home/presentation/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mcquenji_core/mcquenji_core.dart';
+import 'package:shopping_list/modules/auth/auth.dart';
 import 'package:shopping_list/modules/home/home.dart';
 import 'package:shopping_list/utils.dart';
 import 'package:super_cupertino_navigation_bar/super_cupertino_navigation_bar.dart';
@@ -36,6 +37,40 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  void logout() {
+    showCupertinoModalPopup(
+      context: context,
+      builder: (context) {
+        final user = context.watch<UserRepository>();
+
+        return CupertinoActionSheet(
+          title: t.shoppingLists_profile_title.text,
+          message: user.state.when(
+            data: (data) {
+              return t.shoppingLists_profile_message(data?.name ?? "").text;
+            },
+            loading: () => const CupertinoActivityIndicator(),
+            error: (_, __) => const CupertinoActivityIndicator(),
+          ),
+          actions: [
+            CupertinoActionSheetAction(
+              onPressed: () {},
+              child: t.shoppingLists_profile_invite.text,
+            ),
+            CupertinoActionSheetAction(
+              isDestructiveAction: true,
+              onPressed: () async {
+                await user.signOut();
+                Modular.to.navigate("/auth/");
+              },
+              child: t.shoppingLists_profile_logout.text,
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -56,20 +91,29 @@ class _HomeScreenState extends State<HomeScreen> {
                 : CupertinoColors.systemGroupedBackground,
             context,
           ),
-          actions: isCollapsed
-              ? CupertinoButton(
+          leading: isCollapsed
+              ? null
+              : CupertinoButton(
                   onPressed: addShoppingList,
-                  child: const Icon(CupertinoIcons.plus),
-                )
-              : null,
+                  child: t.shoppingLists_new_btn.text,
+                ),
+          actions: !isCollapsed
+              ? null
+              : CupertinoButton(
+                  onPressed: addShoppingList,
+                  child: t.shoppingLists_new_btn.text,
+                ),
           largeTitle: SuperLargeTitle(
             textStyle: context.theme.textTheme.navLargeTitleTextStyle,
             largeTitle: t.shoppingLists_title,
             actions: [
               CupertinoButton(
-                onPressed: addShoppingList,
-                child: t.shoppingLists_new_btn.text,
-              ),
+                child: Icon(
+                  CupertinoIcons.profile_circled,
+                  size: theme.textTheme.navLargeTitleTextStyle.fontSize,
+                ),
+                onPressed: logout,
+              )
             ],
           ),
           searchBar: SuperSearchBar(
@@ -84,7 +128,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ),
-        body: SingleChildScrollView(child: const ShoppingLists()),
+        body: const SingleChildScrollView(child: ShoppingLists()),
       ),
     );
   }
--- lib/modules/home/presentation/widgets/shopping_list_tile.dart
+++ lib/modules/home/presentation/widgets/shopping_list_tile.dart
@@ -6,10 +6,76 @@ import 'package:mcquenji_firebase/mcquenji_firebase.dart';
 import 'package:shopping_list/modules/home/home.dart';
 import 'package:shopping_list/utils.dart';
 
-class ShoppingListTile extends StatelessWidget {
+class ShoppingListTile extends StatefulWidget {
   const ShoppingListTile({super.key, required this.list});
 
   final ShoppingList list;
+
+  @override
+  State<ShoppingListTile> createState() => _ShoppingListTileState();
+}
+
+class _ShoppingListTileState extends State<ShoppingListTile> {
+  void deleteList() {
+    // showCupertinoDialog(
+    //   context: context,
+    //   builder: (_) => CupertinoAlertDialog(
+    //     title: "Delete list".text,
+    //     content: "Are you sure you want to delete '${widget.list.name}'?".text,
+    //     actions: [
+    //       CupertinoDialogAction(
+    //         child: "Cancel".text,
+    //         onPressed: () {
+    //           Navigator.of(context).pop();
+    //         },
+    //       ),
+    //       CupertinoDialogAction(
+    //         child: "Delete".text,
+    //         isDestructiveAction: true,
+    //         onPressed: () {
+    //           context
+    //               .read<ShoppingListsRepository>()
+    //               .deleteList(widget.list.id);
+    //           Navigator.of(context).pop();
+    //         },
+    //       ),
+    //     ],
+    //   ),
+    // );
+
+    context.read<ShoppingListsRepository>().deleteList(widget.list.id);
+    Modular.to.pop();
+  }
+
+  void leaveList() {
+    // showCupertinoDialog(
+    //   context: context,
+    //   builder: (_) => CupertinoAlertDialog(
+    //     title: "Leave list".text,
+    //     content: "Are you sure you want to leave '${widget.list.name}'?".text,
+    //     actions: [
+    //       CupertinoDialogAction(
+    //         child: "Cancel".text,
+    //         onPressed: () {
+    //           Navigator.of(context).pop();
+    //         },
+    //       ),
+    //       CupertinoDialogAction(
+    //         child: "Leave".text,
+    //         isDestructiveAction: true,
+    //         onPressed: () {
+    //           context.read<ShoppingListsRepository>().leaveList(widget.list.id);
+    //           Navigator.of(context).pop();
+    //         },
+    //       ),
+    //     ],
+    //   ),
+    // );
+
+    context.read<ShoppingListsRepository>().leaveList(widget.list.id);
+
+    Modular.to.pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,37 +84,33 @@ class ShoppingListTile extends StatelessWidget {
     return CupertinoContextMenu(
       enableHapticFeedback: true,
       actions: [
-        if (list.owner == auth.currentUser?.uid)
+        if (widget.list.owner == auth.currentUser?.uid)
           CupertinoContextMenuAction(
-            child: "Share".text,
-            trailingIcon: CupertinoIcons.arrowshape_turn_up_right,
+            trailingIcon: CupertinoIcons.person_crop_circle_badge_plus,
             onPressed: () {},
+            child: t.shoppingLists_options_share.text,
           ),
-        if (list.owner == auth.currentUser?.uid)
+        if (widget.list.owner == auth.currentUser?.uid)
           CupertinoContextMenuAction(
-            child: "Delete".text,
             isDestructiveAction: true,
             trailingIcon: CupertinoIcons.trash,
-            onPressed: () {
-              context.read<ShoppingListsRepository>().deleteList(list.id);
-            },
+            onPressed: deleteList,
+            child: t.shoppingLists_options_delete.text,
           ),
-        if (list.members.contains(auth.currentUser?.uid))
+        if (widget.list.members.contains(auth.currentUser?.uid))
           CupertinoContextMenuAction(
-            child: "Leave".text,
             isDestructiveAction: true,
             trailingIcon: CupertinoIcons.person_crop_circle_badge_xmark,
-            onPressed: () {
-              context.read<ShoppingListsRepository>().leaveList(list.id);
-            },
+            onPressed: leaveList,
+            child: t.shoppingLists_options_leave.text,
           ),
       ],
       child: GestureDetector(
         onTap: () {
-          Modular.to.pushNamed("/${list.id}");
+          Modular.to.pushNamed("/${widget.list.id}");
         },
         child: Container(
-          padding: PaddingAll(10),
+          padding: PaddingVertical(10),
           width:
               context.screen.width - DeclarativeEdgeInsets.defaultPadding * 2,
           decoration: BoxDecoration(
@@ -56,21 +118,25 @@ class ShoppingListTile extends StatelessWidget {
               Radius.circular(15),
             ),
             color: CupertinoDynamicColor.resolve(
-              CupertinoColors.tertiarySystemFill,
+              CupertinoColors.systemFill,
               context,
             ),
           ),
-          // child: CupertinoListTile(
-          //   key: ValueKey(list.id),
-          //   title: list.name.text,
-          //   trailing: const CupertinoListTileChevron(),
-          //   additionalInfo: context.t
-          //       .shoppingLists_items(
-          //         list.items.where((e) => !e.checked).length,
-          //       )
-          //       .text,
-          // ),
-          child: FlutterLogo(size: 100),
+          child: CupertinoListTile(
+            key: ValueKey(widget.list.id),
+            title: widget.list.name.text,
+            trailing: const CupertinoListTileChevron(),
+            leading: Icon(
+              widget.list.members.isNotEmpty
+                  ? CupertinoIcons.person_2_fill
+                  : CupertinoIcons.person_fill,
+            ),
+            additionalInfo: context.t
+                .shoppingLists_items(
+                  widget.list.items.where((e) => !e.checked).length,
+                )
+                .text,
+          ),
         ),
       ),
     );
